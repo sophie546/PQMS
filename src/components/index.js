a079f3fc60b3900749b5d96611c6934237e78ed4
Dien Michael Laurente <dienml05@gmail.com>
2025-11-20 23:13:14 +0800

    import and export correction

--- src/components/index.js
+++ src/components/index.js
@@ -8,3 +8,10 @@ export { SearchFilterBar } from './SearchFilterBar';
 export { StatCard, StatTitle, StatNumber, StatIcon, SubText } from './StatComponents';
 export { HeaderPaper, HeaderIcon, HeaderTitle, HeaderSubText, HeaderButton } from './HeaderComponents';
 export { Caption, SubCaption } from './CaptionComponents';
+
+export { default as SettingSideBar } from './general_settings/SettingSideBar';
+export { default as SettingHeader } from './general_settings/SettingHeader';
+export { default as SettingsProfileView } from './general_settings/SettingsProfileView';
+export { default as SettingsPasswordView } from './general_settings/SettingsPasswordView';
+export { default as SettingsProfessionalView } from './general_settings/SettingsProfessionalView';
+export { default as SettingsEditModal } from './general_settings/SettingsEditModal';
